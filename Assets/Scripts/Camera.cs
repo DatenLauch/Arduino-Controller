f0d087635409e70e6054f175951afe88428ca9d0
DatenLauch <103434272+DatenLauch@users.noreply.github.com>
2024-10-18 22:50:47 +0200

    added camera

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField]
+    GameObject objectToFocus;
+    [SerializeField]
+    float cameraDistance;
+    [SerializeField]
+    float cameraHight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        updateCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateCamera();
+    }
+
+    void updateCameraRotation()
+    {
+        Quaternion cameraRotation = objectToFocus.transform.rotation;
+        Quaternion rotationOffset = Quaternion.Euler(0, 90, 0);
+        transform.rotation = cameraRotation * rotationOffset;
+    }
+
+    void updateCameraPosition()
+    {
+        float camPosX = objectToFocus.transform.position.x;
+        float camPosY = objectToFocus.transform.position.y + cameraHight;
+        float camPosZ = objectToFocus.transform.position.z;
+        Vector3 camPosition = new Vector3(camPosX, camPosY, camPosZ) - (transform.forward * cameraDistance);
+        transform.position = camPosition;
+    }
+
+    void updateCamera()
+    {
+        updateCameraRotation();
+        updateCameraPosition();
+    }
+}
