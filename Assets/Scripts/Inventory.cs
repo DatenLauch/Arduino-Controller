e4ebccdd3bd372d90bd0b80b193402d56cbdb5fb
DatenLauch <103434272+DatenLauch@users.noreply.github.com>
2024-10-19 02:57:39 +0200

    added pickup collision

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    int coins;
     // Start is called before the first frame update
     void Start()
     {
-        
+        coins = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Item"))
+        {
+            Destroy(collider.gameObject);
+            coins++;
+            Debug.Log(coins);
+        }
     }
 }
